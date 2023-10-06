5305f10064a8dd29a22adcce6df60f72b161ba00
Miguel <miguelmoreiracr7@gmail.com>
2023-10-06 20:17:12 -0300

    correção da url

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -11,7 +11,7 @@ const Home = () => {
     const [meals, setMeals] = useState<Array<Meal>>([]);
 
     async function getMeals(pesquisa: string) {
-      await fetch(`http://www.themealdb.com/api/json/v1/1/search.php?s=${pesquisa}`)
+      await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${pesquisa}`)
       .then(data => data.json())
       .then(data => {
         setMeals(data.meals)
